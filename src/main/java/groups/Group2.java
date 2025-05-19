ef695198fa3f44475308d2198fe011165a2091a6
Itay <itay2318@gmail.com>
2025-05-19 15:37:46 +0300

    group 2

--- src/main/java/groups/Group2.java
+++ src/main/java/groups/Group2.java
@@ -20,14 +20,14 @@ public class Group2 extends ProgramBase {
     @Override
     public void writeProgram(){
         moveForwardInSeconds(3); //todo remove this line and start coding
-        turnRightInSeconds(0.8); // todo remove this line and start coding//
-        moveForwardInSeconds(2.4);
+        turnRightInSeconds(0.9); // todo remove this line and start coding//
+        moveForwardInSeconds(2.3);
         turnRightInSeconds(0.8);
         moveForwardInSeconds(2.4);
-        turnLeftInSeconds(0.8);
-        moveForwardInSeconds(5);
-        turnLeftInSeconds(0.8);
-        moveForwardInSeconds(5);
+        turnLeftInSeconds(0.9);
+        moveForwardInSeconds(3);
+        turnLeftInSeconds(0.9);
+        moveForwardInSeconds(4);
         turnLeftInSeconds(0.8);
         moveForwardInSeconds(9);
 
