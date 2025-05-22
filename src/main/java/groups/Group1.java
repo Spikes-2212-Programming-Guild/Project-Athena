d76ed1908459483adc41c2710c80c8ac093c2c47
GIL <eingar.gil@gmail.com>
2025-05-22 10:35:10 +0300

    group 1

--- src/main/java/groups/Group1.java
+++ src/main/java/groups/Group1.java
@@ -20,11 +20,11 @@ public class Group1 extends ProgramBase {
     public void writeProgram() {
 
         moveForwardInSeconds(2);
-        turnRightInSeconds(0.5);
+        turnRightInSeconds(0.8);
         moveForwardInSeconds(4);
-        turnLeftInSeconds(0.5);
+        turnLeftInSeconds(0.8);
         moveForwardInSeconds(1);
-        turnLeftInSeconds(0.5);
+        turnLeftInSeconds(0.8);
         moveForwardInSeconds(6);
     }
 }
