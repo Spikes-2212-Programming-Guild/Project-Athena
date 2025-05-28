0cfb16c76b000efbdfdb9bccedc45f3ced38e65a
Noam Werber <nomwerber@gmail.com>
2025-05-28 09:02:07 +0300

    group 2

--- src/main/java/groups/Group2.java
+++ src/main/java/groups/Group2.java
@@ -19,11 +19,11 @@ public class Group2 extends ProgramBase {
 
     @Override
     public void writeProgram() {
-        moveForwardInSeconds(2); //todo remove this line and start coding
-        turnRightInSeconds(1); //todo remove this line and start coding
         moveForwardInSeconds(3); //todo remove this line and start coding
+        turnRightInSeconds(1); //todo remove this line and start coding
+        moveForwardInSeconds(4); //todo remove this line and start coding
         turnLeftInSeconds(1);
-        moveForwardInSeconds(4);
+        moveForwardInSeconds(5);
     }
 }
 
