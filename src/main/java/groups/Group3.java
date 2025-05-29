89893bfb33445c0720b0e902c1c3317c40a6e0c1
Programming Guild <spikes.no2212@gmail.com>
2025-05-29 09:22:54 +0300

    group 3

--- src/main/java/groups/Group3.java
+++ src/main/java/groups/Group3.java
@@ -19,7 +19,13 @@ public class Group3 extends ProgramBase {
 
     @Override
     public void writeProgram() {
-        moveForwardInSeconds(2); //todo remove this line and start coding
-        turnRightInSeconds(3); //todo remove this line and start coding
+        moveForwardInSeconds(2);
+        turnRightInSeconds(2);
+        moveForwardInSeconds(5);
+        turnLeftInSeconds(2);
+        moveForwardInSeconds(3);
+        turnLeftInSeconds(2);
+        moveForwardInSeconds(5);
+        moveForwardInSeconds(4628);
     }
-}
+    }
