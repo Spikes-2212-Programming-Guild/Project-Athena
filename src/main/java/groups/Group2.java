f396a88bc13bd0639e2b6d56ef4598b5df127d79
GIL <eingar.gil@gmail.com>
2025-05-29 08:51:22 +0300

    eee

--- src/main/java/groups/Group2.java
+++ src/main/java/groups/Group2.java
@@ -19,11 +19,11 @@ public class Group2 extends ProgramBase {
 
     @Override
     public void writeProgram() {
-        moveForwardInSeconds(2); //todo remove this line and start coding
+        moveForwardInSeconds(0.5); //todo remove this line and start coding
         turnRightInSeconds(2); //todo remove this line and start coding
-        moveForwardInSeconds(3);
+        moveForwardInSeconds(2.5);
         turnLeftInSeconds(2);
-        moveForwardInSeconds(2);
+        moveForwardInSeconds(1.5);
         turnLeftInSeconds(2);
         moveForwardInSeconds(5);
 
