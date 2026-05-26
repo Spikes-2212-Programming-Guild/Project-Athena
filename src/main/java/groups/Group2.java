81f2730c976741e11728cf141258bf644c16828f
Spikes2212 <spikes.no2212@gmail.com>
2026-05-26 15:34:05 +0300

    group 2

--- src/main/java/groups/Group2.java
+++ src/main/java/groups/Group2.java
@@ -18,15 +18,13 @@ public class Group2 extends ProgramBase {
 
     @Override
     public void writeProgram() {
-        moveForwardInSeconds(3);
+      moveForwardInSeconds(3);
         turnRightInSeconds(2);
-        moveForwardInSeconds(5);
+        moveForwardInSeconds(6);
+        turnLeftInSeconds(2);
         moveForwardInSeconds(3);
         turnLeftInSeconds(3);
-        moveForwardInSeconds(6);
-
-
-
+        moveForwardInSeconds(7);
 
     }
 }
